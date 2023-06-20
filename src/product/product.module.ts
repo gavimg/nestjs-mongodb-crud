bf9c1d221b405f2422df51a0b5ad8e1b9943d295
GavishiddappaMG <gmgadagi@gmail.com>
2023-06-20 22:16:43 +0530

    initial commit

---
+++ src/product/product.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+import { Schema } from 'mongoose';
+import { ProductSchema } from './product.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
+  providers: [ProductService],
+  controllers: [ProductController],
+})
+export class ProductModule {}
---
+++ src/product/product.schema.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema()
+export class Product {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop()
+  category: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
